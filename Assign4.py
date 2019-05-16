682274a98984cf90cc996cb8f0982b2631c4eeaf
swattamw <47372664+swattamw@users.noreply.github.com>
2019-05-16 18:12:58 -0400

    Add files via upload

--- Assign4.py
+++ Assign4.py
@@ -5,27 +5,30 @@ from textblob import TextBlob
 from collections import Counter
 
 
-
+#create a Empty TXT file
 tfc = open("tfc.txt", "w+")
+#get the list of all downloaded files
 tfc_files = glob.glob('/Users/shreyastro/Desktop/Desktop_files/Stevens_Statements/spring_2019/FE595/Assign4/files/*.txt')
+
+# First Script
 with tfc as outfile:
     for file in tfc_files:
         with open(file) as in_file:
             for line in in_file:
                 line = str(line)
-                line = line.strip()
-                if "She's" in line:
+                line = line.strip()#remove spaces
+                if "She's" in line:#first woman sentences and then repeat process for men
                     x = re.split("She", line)
-                    if len(x) >= 3:
+                    if len(x) >= 3:#check for multiple woman sentences
                         line = ""
                         for i in x:
-                            if len(i) > 10:
+                            if len(i) > 10: # This ensure no empty lines are appended
                                 if "She's" not in i:
                                     i = "She" + i
-                                if i[0].islower():
+                                if i[0].islower():#just to convert lower case to upper case
                                     if "she's" in i:
                                         i = "S" + i[1:]
-                                while not re.search(r'(\w|[.])$', i):
+                                while not re.search(r'(\w|[.])$', i):#check for Unecessary Random Characters
                                     i = i[:-1]
                                     i = i.strip()
                                 if re.search(r'\w$', i):
@@ -51,7 +54,7 @@ with tfc as outfile:
                         if re.search(r'^\W', line):
                             line = line[1:]
                             line = line.strip()
-                elif "He's" in line:
+                elif "He's" in line:# repeating for Men
                     x = re.split("He", line)
                     if len(x) >= 3:
                         line = ""
@@ -101,6 +104,7 @@ with tfc as outfile:
                 outfile.write(line)
 
 
+# Second Script For polarity
 fulltext = open("tfc.txt", "r")
 get_word = fulltext.read()
 blob = TextBlob(get_word)
@@ -110,20 +114,17 @@ worstman = " "
 worstwoman = " "
 woman = ""
 man = ""
-for i in blob.sentences:
-    
+for i in blob.sentences: 
     if "She's" in i:
-        sentPol = i.polarity
-        
+        sentPol = i.polarity #Polarity check
         BW = TextBlob(bestwoman).polarity
         WW = TextBlob(worstwoman).polarity
-        if sentPol > BW:
+        if sentPol > BW: #bubble sort technique for Best and worst polarity in Woman sentences
             bestwoman = str(i)
         if sentPol < WW:
             worstwoman = str(i)
     else:
-        
-        sentPol = i.polarity
+        sentPol = i.polarity # Same thing repeat for men
         BM = TextBlob(bestman).polarity
         WM = TextBlob(worstman).polarity
         if sentPol > BM:
@@ -131,9 +132,12 @@ for i in blob.sentences:
         if sentPol < WM:
             worstman = str(i)
 
-print("Best: " + bestman + " " + bestwoman + " They fight crime!")
+print("Best: " + bestman + " " + bestwoman + " They fight crime!") # printing the results
 print("Worst: " + worstman + " " + worstwoman + " They fight crime!")
 
+
+
+# Third Script For 10 most common Lines
 get_lines = get_word.splitlines()
 for lines in get_lines:
     if "She's" in lines:
